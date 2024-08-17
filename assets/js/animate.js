5b6e29be306108468269668d4d3f4d636dc85bf7
Percy <percival.w.bradley@gmail.com>
2024-08-16 18:47:33 -0700

    JS Animation & IMG Shrink Size

---
+++ assets/js/animate.js
@@ -0,0 +1,39 @@
+$(function(){  // $(document).ready shorthand
+    $('.monster').fadeIn('slow');
+  });
+  
+  $(document).ready(function() {
+      
+    setTimeout(function() { 
+        $('.fadein').each( function(i){
+            $(this).animate({'opacity':'1'},1000);  
+        }); 
+    }, 300);
+
+
+    /* Every time the window is scrolled ... */
+        $(window).scroll( function(){
+            /* Check the location of each desired element */
+            $('.hideme').each( function(i){
+                var top_of_object = $(this).position().top + 100;
+                var bottom_of_object = $(this).position().top + $(this).outerHeight();
+                var bottom_of_window = $(window).scrollTop() + $(window).height();
+              
+                /* If the object is completely visible in the window, fade it it */
+                if( bottom_of_window > top_of_object || bottom_of_window > bottom_of_object ){
+                    $(this).animate({'opacity':'1'},1000);  
+                } 
+            }); 
+        });
+
+        $('.hideme').each( function(i){
+            var top_of_object = $(this).position().top + 100;
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > top_of_object || bottom_of_window > bottom_of_object ){
+                $(this).animate({'opacity':'1'},1000);  
+            } 
+        }); 
+    });
